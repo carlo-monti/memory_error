2bac5b03c59c9aa14836ae18cb82f688c33b14c7
Carlo Monti <79301372+carlo-monti@users.noreply.github.com>
2023-08-23 10:37:11 +0200

    Update edit_html.php

--- edit_html.php
+++ edit_html.php
@@ -2,8 +2,9 @@
     $page = $_POST["page"];
     //$text = htmlentities($_POST["plain_html"]);
     $text = str_replace("\n","",$_POST["plain_html"]);
-    $fp = fopen("./archive/".$page, "w");
-    fwrite($fp, $text); 
-    fclose($fp);  
+    //echo $text;
+    $fp = fopen("./archive/".$page, "w") or die("Unable to open file!");
+    $ret = fwrite($fp, $text);
+    fclose($fp);
     header("Location: ./index.php?request=" . $page);
 ?>
